2b2e554bf262fbb54992555319b2b5273e952d9f
NisalMadura <120001738+NisalMadura@users.noreply.github.com>
2024-06-07 23:52:40 +0530

    Second commit

---
+++ src/game_quest/Game_Quest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package game_quest;
+
+
+/**
+ *
+ * @author ndila
+ */
+public class Game_Quest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainMenu menu = new MainMenu();
+        
+        menu.setVisible(true);
+    }
+    
+}
